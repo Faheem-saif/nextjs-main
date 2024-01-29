6d77523095e038942d3c44820900127bd74d8bcc
Faheem-saif <faheem000saif@gmail.com>
2024-01-30 00:34:43 +0500

    solved

--- blog-app/src/app/components/section/section2.tsx
+++ blog-app/src/app/components/section/section2.tsx
@@ -16,7 +16,7 @@ export default function DisplayBlog() {
 }
 
 
-const blogsgrid=(imag:any) => {
+const blogsgrid=(image:any) => {
 return( 
 <div className='flex flex-col m-2 p-2'>
 <Image className='bg-red-700 ' width={300} height={200} src={`/images/${image}.jpg`} alt='noImage'/>
